55164ff4f30b86595419294d60ab84348fee770c
Alexander Akulich <akulichalexander@gmail.com>
2023-02-18 18:03:15 +0300

    Fix the name/ip lookup

--- scripts/lookup-helper.sh
+++ scripts/lookup-helper.sh
@@ -5,4 +5,4 @@ SCRIPTS_PATH=$(dirname $SCRIPT)
 
 . $SCRIPTS_PATH/common.sh
 
-grep -e $TERM -R "$INSTALL_DIR/$LOG_DIR" | cut -d [ -f 2,3,7| cut -d ' ' -f 1,4- | sed 's/:[0-9]*\]/\]/g' | uniq
+grep -e $TERM -R "$LOG_DIR" | cut -d [ -f 2,3,7| cut -d ' ' -f 1,4- | sed 's/:[0-9]*\]/\]/g' | uniq
